44574486d540f9bb2dabf7da41a715e1d5f57d2e
duhowise <duhowise@gmail.com>
2018-04-25 09:43:15 +0000

    added api controllers

---
+++ Src/WebApp/Controllers/FaceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Face")]
+    public class FaceController : Controller
+    {
+       
+        
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]string value)
+        {
+
+
+            try
+            {
+                var url = new Url("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect");
+                var id= await url.WithHeader("Ocp-Apim-Subscription-Key", "1c8723038192418da1e15f8a1f052a9e").WithHeader("COntent-Type", "application/octet-stream")
+                    .SetQueryParams(new { returnFaceId = true, returnFaceLandmarks = true }).PostJsonAsync(Convert.FromBase64String(value)).ReceiveJson<string>();
+                return Ok(id);
+            }
+            catch (Exception e)
+            {
+                return Ok(e);
+            }
+        }
+        
+        
+    }
+}
--- Src/WebApp/wwwroot/js/site.js
+++ Src/WebApp/wwwroot/js/site.js
@@ -1,14 +1,17 @@
 ﻿// Write your Javascript code.
 $(document).ready( function() {
-    $(document).on('change', '.btn-file :file', function() {
-    var input = $(this),
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [label]);
+    var originalFaceId = null;
+    var compareFaceId = null;
+    $(document).on("change", ".btn-file :file", function() {
+        var input = $(this),
+                label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+            input.trigger("fileselect", [label]);
+         
     });
 
-    $('.btn-file :file').on('fileselect', function(event, label) {
+    $(".btn-file :file").on("fileselect", function(event, label) {
         
-        var input = $(this).parents('.input-group').find(':text'),
+        var input = $(this).parents(".input-group").find(":text"),
             log = label;
         
         if( input.length ) {
@@ -18,12 +21,38 @@ $(document).ready( function() {
         }
     
     });
-    function readOriginalURL(input) {
+
+    function getFaceId(image) {
+        fetch("/api/face", {  
+                method: "POST",  
+                headers: {  
+                    'Content-Type': "application/json"  
+                },
+                body: image
+            })
+            .then(function (data) {  
+                console.log("Request success: ", data);  
+            })  
+            .catch(function (error) {  
+                console.log("Request failure: ", error);  
+       });
+        
+
+    }
+
+
+
+
+    function readOriginalUrl(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             
             reader.onload = function (e) {
+                var data = reader.readAsBinaryString(e.target.result.split(","[1]));
+                console.log(data);
                 $('#img-original').attr('src', e.target.result);
+                //originalFaceId = getFaceId(data);
+
             }
             
             reader.readAsDataURL(input.files[0]);
@@ -33,7 +62,7 @@ var readData=null;
 
     // $("#imgCompare").click();
 
-    function readCompareURL(input) {
+    function readCompareUrl(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             
@@ -48,10 +77,10 @@ var readData=null;
     }
 
     $("#imgInp").change(function(){
-        readOriginalURL(this);
+        readOriginalUrl(this);
     }); 	
 
     $("#imgCompare").change(function(){
-        readCompareURL(this);
+        readCompareUrl(this);
     }); 	
 });
