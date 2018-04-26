fdda225b17401c8d0ab401426b05b02be3d844a7
duhowise <duhowise@gmail.com>
2018-04-26 09:52:50 +0000

    Working Detect Api

--- Src/WebApp/Controllers/FaceController.cs
+++ Src/WebApp/Controllers/FaceController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebApp.Controllers
 {
@@ -11,19 +16,20 @@ namespace WebApp.Controllers
     [Route("api/Face")]
     public class FaceController : Controller
     {
-       
-        
+        public FaceController()
+        {
+            
+        }        
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody]string value)
+       [Route("detect")] public async Task<IActionResult> Detect([FromBody]string image)
         {
-
-
+          
             try
             {
                 var url = new Url("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect");
-                var id= await url.WithHeader("Ocp-Apim-Subscription-Key", "1c8723038192418da1e15f8a1f052a9e").WithHeader("COntent-Type", "application/octet-stream")
-                    .SetQueryParams(new { returnFaceId = true, returnFaceLandmarks = true }).PostJsonAsync(Convert.FromBase64String(value)).ReceiveJson<string>();
+                var id= await url.WithHeader("Ocp-Apim-Subscription-Key", "1c8723038192418da1e15f8a1f052a9e")
+                    .SetQueryParams(new { returnFaceId = true, returnFaceLandmarks = true }).PostJsonAsync(image).ReceiveJson<object>();
                 return Ok(id);
             }
             catch (Exception e)
@@ -35,3 +41,4 @@ namespace WebApp.Controllers
         
     }
 }
+
--- Src/WebApp/wwwroot/js/site.js
+++ Src/WebApp/wwwroot/js/site.js
@@ -1,86 +1,106 @@
 ﻿// Write your Javascript code.
-$(document).ready( function() {
+$(document).ready(function() {
     var originalFaceId = null;
     var compareFaceId = null;
-    $(document).on("change", ".btn-file :file", function() {
-        var input = $(this),
-                label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    $(document).on("change",
+        ".btn-file :file",
+        function() {
+            var input = $(this),
+                label = input.val().replace(/\\/g, "/").replace(/.*\//, "");
             input.trigger("fileselect", [label]);
-         
-    });
 
-    $(".btn-file :file").on("fileselect", function(event, label) {
-        
-        var input = $(this).parents(".input-group").find(":text"),
-            log = label;
-        
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) alert(log);
-        }
-    
-    });
+        });
+
+    $(".btn-file :file").on("fileselect",
+        function(event, label) {
+
+            var input = $(this).parents(".input-group").find(":text"),
+                log = label;
+
+            if (input.length) {
+                input.val(log);
+            } else {
+                if (log) alert(log);
+            }
+
+        });
 
     function getFaceId(image) {
-        fetch("/api/face", {  
-                method: "POST",  
-                headers: {  
-                    'Content-Type': "application/json"  
+        fetch("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/octet-stream",
+                    "Ocp-Apim-Subscription-Key":"1c8723038192418da1e15f8a1f052a9e"
                 },
                 body: image
             })
-            .then(function (data) {  
-                console.log("Request success: ", data);  
-            })  
-            .catch(function (error) {  
-                console.log("Request failure: ", error);  
-       });
-        
+            .then(function(data) {
+                console.log("Request success: ", data);
+            })
+            .catch(function(error) {
+                console.log("Request failure: ", error);
+            });
+
 
     }
 
+    function getFaceIdLocal(image) {
+        fetch("/api/face/detect",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/octet-stream"
+                },
+                body: JSON.stringify(image)
+            })
+            .then(function(data) {
+                console.log("Request success: ", data);
+            })
+            .catch(function(error) {
+                console.log("Request failure: ", error);
+            });
 
+
+    }
 
 
     function readOriginalUrl(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-            
-            reader.onload = function (e) {
-                var data = reader.readAsBinaryString(e.target.result.split(","[1]));
-                console.log(data);
-                $('#img-original').attr('src', e.target.result);
-                //originalFaceId = getFaceId(data);
-
-            }
-            
             reader.readAsDataURL(input.files[0]);
+            reader.onload = function(e) {
+                $("#img-original").attr("src", e.target.result);
+
+            //var imageData = e.target.result.split(",")[1];
+               var faceId = getFaceId(input.files[0]);
+            };
+
         }
     }
-var readData=null;
 
     // $("#imgCompare").click();
 
     function readCompareUrl(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-            
-            reader.onload = function (e) {
-                $('#img-compare').attr('src', e.target.result);
-            }
-            
+
+            reader.onload = function(e) {
+                $("#img-compare").attr("src", e.target.result);
+                var imageData = e.target.result.split(",")[1];
+                var faceId = getFaceId(imageData);
+            };
             reader.readAsDataURL(input.files[0]);
-         
+
         }
-        input=null;
     }
 
-    $("#imgInp").change(function(){
-        readOriginalUrl(this);
-    }); 	
 
-    $("#imgCompare").change(function(){
+    $("#imgInp").change(function() {
+        readOriginalUrl(this);
+    });
+
+    $("#imgCompare").change(function() {
         readCompareUrl(this);
-    }); 	
+    });
 });
